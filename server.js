6b863c59bb2a13510bd3f3110d81496c58f0a077
Mark Haley <markhaley44@gmail.com>
2019-01-07 20:41:28 -0500

    updates to pack.json & server,js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const router = require('./routes/index')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', router)
+app.set('view engine', 'hbs')
+
+
+// process.env.PORT is necessary for deployment to Heroku
+// If environment variable is not provided, default to 3000
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
