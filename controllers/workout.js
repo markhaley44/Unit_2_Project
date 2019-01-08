7d07719dc5979295ad7203a96b273d92fa08f972
Mark Haley <markhaley44@gmail.com>
2019-01-08 15:39:05 -0500

    modified connection

--- controllers/workout.js
+++ controllers/workout.js
@@ -1,4 +1,4 @@
-const Workout = require('..models/Workout')
+const Workout = require('../models/Workout')
 
 const WorkoutController = {
     index: (req, res) => {
--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect("mongodb://localhost/workout").then(() => {
     console.log("MONGODB is now connected")
 })
 
