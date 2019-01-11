82308bb52f065c28a0c1ccc4c6a86f4c7b67624f
Mark Haley <markhaley44@gmail.com>
2019-01-11 13:01:13 -0500

    connect for Heroku

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost/workout").then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MONGODB is now connected")
 })
 
